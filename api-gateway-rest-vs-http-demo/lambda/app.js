c2af53bf3974537d490daa5e121996de71d099c6
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-06-01 16:59:13 -0300

    Fix apis by adjusting lambda handler

--- api-gateway-rest-vs-http-demo/lambda/app.js
+++ api-gateway-rest-vs-http-demo/lambda/app.js
@@ -1,7 +1,14 @@
 exports.handler = async (event) => {
-    console.log(`Event ${JSON.stringify(event)}`)
+    console.log(`Event ${JSON.stringify(event)}`);
+    
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Message from Lambda"})
-    }
-}
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+            message: "Message from Lambda",
+            timestamp: new Date().toISOString()
+        })
+    };
+};

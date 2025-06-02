04b175a0abdf621cb53dc8e5da05699d0adbf6b8
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-06-01 21:01:28 -0300

    Add query string parameters

--- api-gateway-rest-vs-http-demo/lambda/app.js
+++ api-gateway-rest-vs-http-demo/lambda/app.js
@@ -1,13 +1,18 @@
 exports.handler = async (event) => {
     const name = (event.pathParameters && event.pathParameters.name) || "world";
-    
+     
+    const queryParameters = event.queryStringParameters || {}
+    const filter = queryParameters.filter || ""
+    const size = queryParameters.size || ""
+
     return {
         statusCode: 200,
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ 
-            message: `Hello, ${name}`,
+            message: `Hello, ${name}. With filter ${filter} and size ${size}`,
+            event: event
         })
     };
 };

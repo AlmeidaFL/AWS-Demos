6b9cab4cc20f5eaaf87d15039dcad67fbc5214b4
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-06-11 23:21:57 -0300

    Add health check endpoint and configuration for Elastic Beanstalk

--- eb-node-basic/index.js
+++ eb-node-basic/index.js
@@ -4,6 +4,11 @@ const port = process.env.PORT || 3000
 const message = process.env.CUSTOM_MESSAGE || 'Message not found on env.config'
 
 http.createServer((req, res) => {
+    if (req.url === '/health'){
+        res.writeHead(200, { "content-type": 'application/json'})
+        return res.end(JSON.stringify({status: 'ok'}))
+    }
+
     res.writeHead(200, { 'content-type': 'text/plain'})
     res.end(`This is a custom message: ${message}`)
 }).listen(port)

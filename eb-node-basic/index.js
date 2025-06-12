7f5ede9ae9f62a30babe30e7457b57cdbd9e96c9
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-06-11 22:17:27 -0300

    Fix typo in environment configuration and update server response message

--- eb-node-basic/index.js
+++ eb-node-basic/index.js
@@ -1,10 +1,11 @@
 const http = require('http')
 
 const port = process.env.PORT || 3000
+const message = process.env.CUSTOM_MESSAGE || 'Message not found on env.config'
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'content-type': 'text/plain'})
-    res.end('Hello, World')
+    res.end(`This is a custom message: ${message}`)
 }).listen(port)
 
 console.log(`App running on port ${port}`)
